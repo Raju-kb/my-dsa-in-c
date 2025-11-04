17c6a4e23facffb697dc756b5a695c725f8297a7
Raju k b <kbraju788@gmail.com>
2025-11-04 22:52:19 +0530

    Add delete-at-end.c to print array elements

---
+++ delete-at-end.c
@@ -0,0 +1,15 @@
+// delete at end
+#include <stdio.h>
+
+int main() {
+    printf("NAME :Raju K B\nUSN:1SV24CS\n");
+    int arr[5]={1,2,3,4,5};
+    int n,i;
+    n=5;
+    n--;   
+
+    for(i=0;i<n;i++)
+        printf("arr[%d]=%d\n",i,arr[i]);
+
+    return 0;
+}
